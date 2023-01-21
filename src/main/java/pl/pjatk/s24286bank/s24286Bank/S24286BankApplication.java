23f493959a40a25ac72f84cc238b5e2fc2284abf
kacper-miklewicz <kacper.miklewicz@gmail.com>
2023-01-21 17:33:21 +0100

    initial commit

---
+++ src/main/java/pl/pjatk/s24286bank/s24286Bank/S24286BankApplication.java
@@ -0,0 +1,17 @@
+package pl.pjatk.s24286bank.s24286Bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S24286BankApplication {
+
+	public S24286BankApplication() {
+//		System.out.println("test");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(S24286BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/s24286bank/s24286Bank/S24286BankApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.s24286bank.s24286Bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S24286BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
